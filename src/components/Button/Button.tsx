01fb44b1d509b5dbb5abb9e46ec230e302fc36c6
이윤슬 <ysisys0202@gmail.com>
2025-01-13 00:20:10 +0900

    💄 Button 컴포넌트 추가

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,54 @@
+import {
+  ButtonHTMLAttributes,
+  forwardRef,
+  ReactNode,
+  ElementType,
+} from "react";
+import { combineClassNames } from "@/utils/common";
+import { buttonStyles } from "./style.css";
+
+export type ButtonProps = {
+  variant?: "outlined" | "contained" | "text";
+  size?: "sm" | "md" | "lg";
+  theme?: "primary" | "success" | "error" | "warning" | "light" | "dark";
+  as?: ElementType;
+  children: ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const defaultButtonProps = {
+  variant: "contained" as ButtonProps["variant"],
+  size: "md" as ButtonProps["size"],
+  theme: "primary" as ButtonProps["theme"],
+  as: "button",
+};
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      variant = defaultButtonProps.variant,
+      size = defaultButtonProps.size,
+      theme = defaultButtonProps.theme,
+      as: Component = defaultButtonProps.as,
+      children,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    const classNames = combineClassNames(
+      className,
+      buttonStyles({
+        variant,
+        theme,
+        size,
+      })
+    );
+    return (
+      <Component ref={ref} className={classNames} {...props}>
+        {children}
+      </Component>
+    );
+  }
+);
+
+export default Button;
---
+++ src/components/Button/style.css.ts
@@ -0,0 +1,106 @@
+import { recipe } from "@vanilla-extract/recipes";
+import * as colors from "@/constants/colors";
+import { variantStyles as typographyVariantStyles } from "@/components/Typography/style.css";
+
+const sizeStyles = {
+  sm: {
+    borderRadius: "4px",
+    padding: "4px 8px",
+    ...typographyVariantStyles.text2,
+  },
+  md: {
+    borderRadius: "6px",
+    padding: "6px 14px",
+    ...typographyVariantStyles.text1,
+  },
+  lg: {
+    borderRadius: "8px",
+    padding: "8px 16px",
+    ...typographyVariantStyles.title5,
+  },
+};
+
+const themeStyles = {
+  primary: {
+    backgroundColor: colors.blue[500],
+    borderColor: colors.blue[500],
+    color: colors.blue[500],
+  },
+  success: {
+    backgroundColor: colors.green[500],
+    borderColor: colors.green[500],
+    color: colors.green[500],
+  },
+  error: {
+    backgroundColor: colors.red[500],
+    borderColor: colors.red[500],
+    color: colors.red[500],
+  },
+  warning: {
+    backgroundColor: colors.yellow[500],
+    borderColor: colors.yellow[500],
+    color: colors.yellow[500],
+  },
+  light: {
+    backgroundColor: colors.white,
+    borderColor: colors.white,
+    color: colors.white,
+  },
+  dark: {
+    backgroundColor: colors.gray[600],
+    borderColor: colors.gray[600],
+    color: colors.gray[600],
+  },
+};
+
+const variantStyles = {
+  outlined: {
+    backgroundColor: "transparent",
+  },
+  contained: {
+    color: colors.white,
+  },
+  text: {
+    border: "none",
+    backgroundColor: "transparent",
+  },
+};
+
+export const buttonStyles = recipe({
+  base: {
+    display: "inline-flex",
+    justifyContent: "center",
+    alignItems: "center",
+    boxSizing: "border-box",
+    cursor: "pointer",
+    border: "1px solid",
+    outline: "none",
+    borderWidth: 1,
+    transition: "opacity 100ms ease-in-out",
+
+    ":hover": {
+      opacity: 0.8,
+    },
+  },
+  variants: {
+    size: sizeStyles,
+    theme: themeStyles,
+    variant: variantStyles,
+  },
+  compoundVariants: [
+    {
+      variants: {
+        variant: "contained",
+        theme: "light",
+      },
+      style: {
+        color: colors.gray[500],
+      },
+    },
+  ],
+  defaultVariants: {
+    size: "md",
+    theme: "primary",
+    variant: "contained",
+  },
+});
