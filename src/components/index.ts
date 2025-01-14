1e6df1dc122d4678c94ab783078bffc2be80ce0c
이윤슬 <ysisys0202@gmail.com>
2025-01-14 22:59:45 +0900

    🔥 파일 모두 삭제

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Typography";
-export * from "./Button";
-export * from "./Divider";
-export * from "./Tag";
-export * from "./Input";
--- src/utils/common.ts
+++
@@ -1,5 +0,0 @@
-export const combineClassNames = (
-  ...classes: (string | undefined | null | false)[]
-): string => {
-  return classes.filter(Boolean).join(" ");
-};
