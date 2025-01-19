83ab8d115a5b4c93c7b98ae427089a00929d70ac
이윤슬 <ysisys0202@gmail.com>
2025-01-20 07:21:14 +0900

    🐛 svg 빌드 안되는 현상 수정 (dynamic import 제거)

--- src/components/Icon/index.ts
+++ src/components/Icon/index.ts
@@ -1,1 +1,15 @@
 export * from "./Icon";
+
+import arrow from "./svgs/arrow.svg?react";
+import book from "./svgs/book.svg?react";
+import chevron from "./svgs/chevron.svg?react";
+import infomation from "./svgs/infomation.svg?react";
+import warning from "./svgs/warning.svg?react";
+
+export const svgs = {
+  arrow,
+  book,
+  chevron,
+  infomation,
+  warning,
+};
--- src/components/Icon/useIcon.tsx
+++ src/components/Icon/useIcon.tsx
@@ -1,31 +1,15 @@
 import { useEffect, useState } from "react";
-import { SvgrComponent } from "./Icon";
+import { IconProps, SvgrComponent } from "./Icon";
+import { svgs } from "./index";
 
-const loadSvgModule = (
-  name: string
-): Promise<{
-  default: SvgrComponent;
-} | null> => {
-  try {
-    return import(`./svgs/${name}.svg?react`);
-  } catch (error) {
-    throw new Error(`${name} 아이콘이 존재하지 않습니다.`);
-  }
-};
-
-export const useIcon = (name: string) => {
+export const useIcon = (name: IconProps["name"]) => {
   const [icon, setIcon] = useState<SvgrComponent | null>(null);
 
   useEffect(() => {
-    const assignIcon = async () => {
-      const module = await loadSvgModule(name);
-
-      if (module && module.default) {
-        setIcon(() => module.default);
-      }
-    };
-
-    assignIcon();
+    if (!svgs[name]) {
+      console.error(`${name} 아이콘이 존재하지 않습니다.`);
+    }
+    setIcon(() => svgs[name]);
   }, [name]);
 
   return { Icon: icon };
