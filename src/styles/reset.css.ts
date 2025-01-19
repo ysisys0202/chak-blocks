dc55dff7187b93cad2eda1095e2e170548583515
이윤슬 <ysisys0202@gmail.com>
2025-01-20 05:09:31 +0900

    💄 reset css 추가

---
+++ src/styles/reset.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css";
+
+export const resetStyle = style({
+  margin: 0,
+  padding: 0,
+  boxSizing: "border-box",
+  border: "none",
+  outline: "none",
+  listStyle: "none",
+});
