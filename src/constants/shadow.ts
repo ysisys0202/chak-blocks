5328238bacdc1f6be0cfcde543c067e13134f81f
이윤슬 <ysisys0202@gmail.com>
2025-01-20 11:15:29 +0900

    💄 shadow 상수 추가

---
+++ src/constants/shadow.ts
@@ -0,0 +1,6 @@
+export const shadow = {
+  primary:
+    "0 6px 16px 0 rgba(0, 0, 0, 0.08), 0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 9px 28px 8px rgba(0, 0, 0, 0.05)",
+  secondary:
+    "0 1px 2px 0 rgba(0, 0, 0, 0.03), 0 1px 6px -1px rgba(0, 0, 0, 0.02), 0 2px 4px 0 rgba(0, 0, 0, 0.02)",
+};
